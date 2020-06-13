6830cb1de91422678d3e8287cca617301804f5fe
Justin Tran <tranjustin2@gmail.com>
2020-06-13 13:50:38 -0400

    Updated for Python3

--- tracker.py
+++ tracker.py
@@ -1,33 +1,19 @@
-import json, urllib, urllib2, os, time, string
+import json, urllib.request, os, time
 
-'''
-This function runs in an infinite loop. It uses the 
-try / except clause as a error handling measure. It makes a 
-request to the API to return some json data. The data is 
-then sorted by the Cog name key and printed to the terminal. 
-The process repeats after 9.5 seconds due to API limitations.
-'''
-
-while True:
-  try:
-    page = urllib2.urlopen('http://api.ttr-invasions.com/json/invasionlist/')
-    data = sorted(json.load(page), key = lambda i: (i['invasion_cog']))
+while(1):
     os.system('cls')
+    page = urllib.request.urlopen('http://api.ttr-invasions.com/json/invasionlist/')
+    data = sorted(json.load(page), key = lambda i: (i['invasion_cog']))
 
     for line in data:
-      cog = line.get("invasion_cog")
-      cogtype = line.get("invasion_cogType")
-      shard = line.get("invasion_district")
-      progress = line.get("invasion_progress")
-      remainder = line.get("invasion_remaining")
-      if cog == "Mover &amp; Shaker":
-        cog = "Mover & Shaker"
-        
-      print ('{:<17} {:<9} {:<14} {:<10} {:}'.format(cog, cogtype, shard, progress, remainder))
+        cog = line.get("invasion_cog")
+        cogtype = line.get("invasion_cogType")
+        shard = line.get("invasion_district")
+        progress = line.get("invasion_progress")
+        remainder = line.get("invasion_remaining")
+        if cog == "Mover &amp; Shaker":
+            cog = "Mover & Shaker"
 
-    time.sleep(9.5)
+        print(f'{cog:<17} {cogtype:<9} {shard:<14} {progress:<10} {remainder:}', flush = True)
 
-  except:
-    pass
-
-
+    time.sleep(9.99)
